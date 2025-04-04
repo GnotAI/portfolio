26f8f78b7e98d55d5dcd158ed6d5ab9487d9c6e6
GnotAI <alleluiabriggs@gmail.com>
2025-04-04 11:58:09 +0100

    upd: projects

--- src/consts.js
+++ src/consts.js
@@ -2,7 +2,7 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
   pc: `This is my PC. I work mostly don't work these days.
-    I've made a couple of simple CRUD backends in various languages and frameworks. I also like Golang, Python and C# (but we don't talk about that one). Anyway regardless of the language, I just like programming.
+    I've made a couple of backends in various languages and frameworks. I also like Golang, Python and C++ (but we don't talk about that one). Anyway regardless of the language, I just like programming and I'm looking for git repos to contribute to.
     Here is my <a href="https://github.com/GnotAI" target="_blank">Github</a>!`,
   "cs-degree": `This is my CS degree. Or where it should be when I get it.`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
@@ -15,12 +15,16 @@ export const dialogueData = {
   resume: `Nothing here yet. Try another time.`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching the tutorial <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  Kaboom.js by watching the tutorial <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.
+  Recent projects include:
+  Skill trade backend practice: <a href="https://github.com/GnotAI/skilltrade" target="_blank">Skill trade API</a>,
+  Round robin load balancer for servers: <a href="https://github.com/GnotAI/loadbalancer-practice" target="_blank">Load balancer</a>
+  `,
   library: `There are a lot of programming books on my shelves (more like one but who cares anyway).`,
   exit: `If you want to exit the portfolio, just close the tab.`,
 };
 
 
-
 // const resumeDraft = `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
+
   // Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`
